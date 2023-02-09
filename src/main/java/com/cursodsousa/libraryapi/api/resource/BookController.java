33bfb22c0ad37572926b2de9196afd5bd05af862
Ilvan-J <ilvanlopesjardim110@gmail.com>
2023-02-09 17:12:33 -0300

    Teste controller deve lançar de erro de validação quando não houver dados suficientes para criação do livro

--- src/main/java/com/cursodsousa/libraryapi/api/resource/BookController.java
+++ src/main/java/com/cursodsousa/libraryapi/api/resource/BookController.java
@@ -1,11 +1,18 @@
 package com.cursodsousa.libraryapi.api.resource;
 
 import com.cursodsousa.libraryapi.api.dto.BookDTO;
+import com.cursodsousa.libraryapi.api.exception.ApiErrors;
 import com.cursodsousa.libraryapi.model.entity.Book;
 import com.cursodsousa.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -21,9 +28,16 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create( @RequestBody BookDTO dto) {
+    public BookDTO create( @RequestBody @Valid BookDTO dto) {
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErrors(bindingResult);
     }
 }
