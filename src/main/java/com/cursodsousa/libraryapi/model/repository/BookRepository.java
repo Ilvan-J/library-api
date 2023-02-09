729fbd020a2fc9976fcf89f0bfe0be8e5c0f1901
Ilvan-J <ilvanlopesjardim110@gmail.com>
2023-02-09 16:35:13 -0300

    Teste service criação de um livro

---
+++ src/main/java/com/cursodsousa/libraryapi/model/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.cursodsousa.libraryapi.model.repository;
+
+import com.cursodsousa.libraryapi.model.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
---
+++ src/main/java/com/cursodsousa/libraryapi/service/impl/BookServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cursodsousa.libraryapi.service.impl;
+
+import com.cursodsousa.libraryapi.model.entity.Book;
+import com.cursodsousa.libraryapi.model.repository.BookRepository;
+import com.cursodsousa.libraryapi.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private BookRepository respository;
+
+    public BookServiceImpl(BookRepository respository) {
+        this.respository = respository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return respository.save(book);
+    }
+}
