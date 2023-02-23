92b99c3b67cb7194231793905bc92031ac9ecad8
Ilvan-J <ilvanlopesjardim110@gmail.com>
2023-02-23 12:09:16 -0300

    Configuração do servidor MailTrap para envio de emails

---
+++ src/main/java/com/ilvan/libraryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ilvan.libraryapi.service.impl;
+
+import com.ilvan.libraryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+    @Override
+    public void sendMails(String message, List<String> mailsList) {
+
+    }
+}
